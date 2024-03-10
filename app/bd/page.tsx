2c8b4916a7c101e0fedbc095ad3a5c3b037f4d72
sagarmusabbir <sagarmusabbir@gmail.com>
2024-03-10 11:42:33 +0600

    added-paths

--- app/bd/page.tsx
+++ app/bd/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function USPage() {
+export default function BDPage() {
   return (
     <main className="flex flex-row w-screen h-screen justify-center items-center space-x-10 py-10">
-      <h1 className="text-4xl items-center">US</h1>
+      <h1 className="text-4xl items-center">BD</h1>
       <h1 className="text-4xl items-center">My Home Page</h1>
     </main>
   );
--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,20 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const us = "US";
+const bd = "BD";
+const gb = "GB";
+const ae = "AE";
 
 export default function middleware(req: NextRequest) {
   const country = req.geo?.country;
 
   if (country === us) {
     return NextResponse.redirect("./us");
+  } else if (country === bd) {
+    return NextResponse.redirect("./bd");
+  } else if (country === ae) {
+    return NextResponse.redirect("./ae");
+  } else if (country === gb) {
+    return NextResponse.redirect("./gb");
   }
 }
